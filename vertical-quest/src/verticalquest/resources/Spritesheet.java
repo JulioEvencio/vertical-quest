5781b5758f9e27c24ab84d7a937293df6d756806
JulioEvencio <julioigreja.dev@gmail.com>
2024-01-18 20:59:56 -0300

    refactor: Removing unused codes

--- vertical-quest/src/verticalquest/resources/Spritesheet.java
+++ vertical-quest/src/verticalquest/resources/Spritesheet.java
@@ -9,26 +9,18 @@ import verticalquest.strings.StringError;
 
 public class Spritesheet {
 
-	private static final BufferedImage spritesheet;
 	private static final BufferedImage spritesheetGUI;
 
 	static {
-		BufferedImage auxSpritesheet = null;
 		BufferedImage auxSpritesheetGUI = null;
 
 		try {
-			auxSpritesheet = ImageIO.read(Spritesheet.class.getResource("/gui/gui.png"));
 			auxSpritesheetGUI = ImageIO.read(Spritesheet.class.getResource("/gui/gui.png"));
 		} catch (Exception e) {
 			Game.exitWithError(StringError.ERROR_LOADING_SPRITES.getValue());
 		}
 
-		spritesheet = auxSpritesheet;
 		spritesheetGUI = auxSpritesheetGUI;
-	}
-
-	public static BufferedImage getSprite(int x, int y, int width, int height) {
-		return Spritesheet.spritesheet.getSubimage(x, y, width, height);
 	}
 
 	public static BufferedImage getSpriteGUI(int x, int y, int width, int height) {
