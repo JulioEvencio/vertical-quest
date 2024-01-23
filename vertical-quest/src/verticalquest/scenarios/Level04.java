835dc8657e24e3f91ab1321ba2968ce8c6894fa6
JulioEvencio <julioigreja.dev@gmail.com>
2024-01-23 00:45:15 -0300

    update: level 04

--- vertical-quest/src/verticalquest/scenarios/Level04.java
+++ vertical-quest/src/verticalquest/scenarios/Level04.java
@@ -2,29 +2,30 @@ package verticalquest.scenarios;
 
 import java.awt.Color;
 
-import verticalquest.Game;
-import verticalquest.entities.BlockRed;
 import verticalquest.entities.Player;
-import verticalquest.entities.Portal;
-import verticalquest.entities.ZoneSpawn;
 import verticalquest.strings.StringLevel;
-import verticalquest.tiles.Floor;
 import verticalquest.utils.StringRender;
 
 public class Level04 extends Scenario {
 
 	public Level04(Player player) {
-		super(Game.WIDTH * 2, Game.HEIGHT, new ZoneSpawn(100, Game.HEIGHT - 200), new Portal(Game.WIDTH * 2 - 150, Game.HEIGHT - 420), player);
+		super(player);
+	}
 
-		for (int i = 6; i < (super.width / 50); i++) {
-			super.blockReds.add(new BlockRed(50 * i, super.height - 100));
-		}
-		
-		super.tiles.add(new Floor(50, super.height - 100));
-		super.tiles.add(new Floor(100, super.height - 100));
-		super.tiles.add(new Floor(150, super.height - 100));
-		super.tiles.add(new Floor(200, super.height - 100));
-		super.tiles.add(new Floor(250, super.height - 100));
+	@Override
+	protected void initializeLevel() {
+		super.map = new char[][] {
+			{'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'},
+			{'W', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', 'S', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', ' ', 'J', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', ' ', 'W'},
+			{'W', 'F', 'F', 'F', 'F', 'F', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'F', 'F', 'F', 'W'},
+			{'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'}
+		};
 	}
 
 	@Override
@@ -34,13 +35,8 @@ public class Level04 extends Scenario {
 	}
 
 	@Override
-	protected void playerSetPosition() {
-		super.player.setPosition(150, super.height - 150);
-	}
-
-	@Override
 	protected void nextLevel() {
-		Game.restart(new Level04(super.player));
+		// Game.restart(new Level04(super.player));
 	}
 
 	@Override
