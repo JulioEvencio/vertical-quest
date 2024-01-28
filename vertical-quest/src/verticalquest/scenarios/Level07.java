8176de843e285bc82e6d8f880f37f25e74e2d81a
JulioEvencio <julioigreja.dev@gmail.com>
2024-01-28 14:49:44 -0300

    added: level 07

---
+++ vertical-quest/src/verticalquest/scenarios/Level07.java
@@ -0,0 +1,46 @@
+package verticalquest.scenarios;
+
+import java.awt.Color;
+
+import verticalquest.entities.Player;
+import verticalquest.strings.StringLevel;
+import verticalquest.utils.StringRender;
+
+public class Level07 extends Scenario {
+
+	public Level07(Player player) {
+		super(player);
+	}
+
+	@Override
+	protected void initializeLevel() {
+		super.map = new char[][] {
+			{'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'},
+			{'W', 'S', ' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', 'J', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', 'F', 'F', 'F', ' ', ' ', ' ', 'F', ' ', ' ', ' ', ' ', ' ', 'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', 'F', 'F', 'F', ' ', ' ', ' ', 'F', ' ', ' ', ' ', ' ', ' ', 'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', 'F', 'F', 'F', ' ', ' ', ' ', 'F', ' ', ' ', 'F', ' ', ' ', 'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', 'F', 'F', 'F', ' ', ' ', ' ', 'F', ' ', ' ', 'F', ' ', ' ', 'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', ' ', 'W'},
+			{'W', 'F', 'F', 'F', ' ', ' ', ' ', 'F', ' ', ' ', 'F', ' ', ' ', 'F', ' ', ' ', 'F', 'F', 'F', ' ', ' ', ' ', ' ', 'F', 'F', 'F', 'W'},
+			{'W', 'F', 'F', 'F', 'R', 'R', 'R', 'F', 'R', 'R', 'F', 'R', 'R', 'F', 'R', 'R', 'F', 'F', 'F', 'R', 'R', 'R', 'R', 'F', 'F', 'F', 'W'},
+			{'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'}
+		};
+	}
+
+	@Override
+	protected void setStrings() {
+		super.strings.add(new StringRender(StringLevel.LEVEL_07.getValue(), 80, 80, Color.WHITE));
+	}
+
+	@Override
+	protected void nextLevel() {
+		// Game.restart(new Level08(super.player));
+	}
+
+	@Override
+	protected Scenario getCurrentScenario() {
+		return new Level07(super.player);
+	}
+
+}
