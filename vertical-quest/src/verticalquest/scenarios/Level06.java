3b80a5e52ae5e94ba51fa589a477222328564ebe
JulioEvencio <julioigreja.dev@gmail.com>
2024-01-28 14:38:56 -0300

    added: level 06

---
+++ vertical-quest/src/verticalquest/scenarios/Level06.java
@@ -0,0 +1,46 @@
+package verticalquest.scenarios;
+
+import java.awt.Color;
+
+import verticalquest.entities.Player;
+import verticalquest.strings.StringLevel;
+import verticalquest.utils.StringRender;
+
+public class Level06 extends Scenario {
+
+	public Level06(Player player) {
+		super(player);
+	}
+
+	@Override
+	protected void initializeLevel() {
+		super.map = new char[][] {
+			{'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'},
+			{'W', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', ' ', ' ', ' ', ' ', 'R', 'R', ' ', 'R', 'R', ' ', 'R', 'R', ' ', 'R', 'R', ' ', ' ', ' ', 'W'},
+			{'W', 'S', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', 'J', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', ' ', 'W'},
+			{'W', 'F', 'F', 'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'F', 'F', 'F', 'W'},
+			{'W', 'F', 'F', 'F', ' ', 'R', ' ', ' ', 'R', ' ', ' ', 'R', ' ', ' ', 'R', ' ', ' ', 'F', 'F', 'F', 'W'},
+			{'W', 'F', 'F', 'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'F', 'F', 'F', 'W'},
+			{'W', 'F', 'F', 'F', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'F', 'F', 'F', 'W'},
+			{'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'}
+		};
+	}
+
+	@Override
+	protected void setStrings() {
+		super.strings.add(new StringRender(StringLevel.LEVEL_06.getValue(), 80, 80, Color.WHITE));
+	}
+
+	@Override
+	protected void nextLevel() {
+		// Game.restart(new Level07(super.player));
+	}
+
+	@Override
+	protected Scenario getCurrentScenario() {
+		return new Level06(super.player);
+	}
+
+}
