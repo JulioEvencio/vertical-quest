0faaae1a86dd754adb75c3117de4ba0bea754a94
JulioEvencio <julioigreja.dev@gmail.com>
2024-01-30 22:30:27 -0300

    added: level 10

--- vertical-quest/src/verticalquest/scenarios/Level09.java
+++ vertical-quest/src/verticalquest/scenarios/Level09.java
@@ -2,6 +2,7 @@ package verticalquest.scenarios;
 
 import java.awt.Color;
 
+import verticalquest.Game;
 import verticalquest.entities.Player;
 import verticalquest.strings.StringLevel;
 import verticalquest.utils.StringRender;
@@ -35,7 +36,7 @@ public class Level09 extends Scenario {
 
 	@Override
 	protected void nextLevel() {
-		// Game.restart(new Level10(super.player));
+		Game.restart(new Level10(super.player));
 	}
 
 	@Override
---
+++ vertical-quest/src/verticalquest/scenarios/Level10.java
@@ -0,0 +1,46 @@
+package verticalquest.scenarios;
+
+import java.awt.Color;
+
+import verticalquest.entities.Player;
+import verticalquest.strings.StringLevel;
+import verticalquest.utils.StringRender;
+
+public class Level10 extends Scenario {
+
+	public Level10(Player player) {
+		super(player);
+	}
+
+	@Override
+	protected void initializeLevel() {
+		super.map = new char[][] {
+			{'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'},
+			{'W', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', 'S', ' ', ' ', ' ', ' ', ' ', ' ', 'C', ' ', ' ', 'C', ' ', ' ', 'C', ' ', ' ', 'C', ' ', 'W'},
+			{'W', ' ', ' ', 'J', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', 'F', 'F', 'F', 'F', 'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', ' ', ' ', ' ', ' ', 'F', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', ' ', 'W'},
+			{'W', ' ', 'P', ' ', ' ', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', ' ', 'W'},
+			{'W', 'F', 'F', 'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'W'},
+			{'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', ' ', 'W'},
+			{'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'}
+		};
+	}
+
+	@Override
+	protected void setStrings() {
+		super.strings.add(new StringRender(StringLevel.LEVEL_10.getValue(), 80, 80, Color.WHITE));
+	}
+
+	@Override
+	protected void nextLevel() {
+		// Code
+	}
+
+	@Override
+	protected Scenario getCurrentScenario() {
+		return new Level10(super.player);
+	}
+
+}
